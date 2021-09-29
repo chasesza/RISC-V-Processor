f5ebe022fe67e073a268e740b97d22bd8b7ab41c
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-09-29 19:57:46 -0400

    2_1 mux

---
+++ sources_1/new/mux_2_1.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/28/2021 08:46:31 PM
+-- Design Name: 
+-- Module Name: mux_2_1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux_2_1 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           sel : in STD_LOGIC;
+           q : out STD_LOGIC);
+end mux_2_1;
+
+architecture RTL of mux_2_1 is
+
+begin
+    
+    q <= (a AND sel) OR (b AND (NOT sel) );
+
+end RTL;
