de46ec9ef1da39dac1ce4af5545282ded4ecd990
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-10-09 00:00:33 -0400

    ALU inputs from bit 31 to bit 12

--- sources_1/new/alu.vhd
+++ sources_1/new/alu.vhd
@@ -32,24 +32,124 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity alu is
-    Port ( a : in STD_LOGIC_VECTOR (0 downto 0);
-           b : in STD_LOGIC_VECTOR (0 downto 0);
-           arith : in STD_LOGIC;
-           add_n_sub : in STD_LOGIC;
-           adder : out STD_LOGIC_VECTOR (0 downto 0);
-           s_r : out STD_LOGIC_VECTOR (0 downto 0);
-           s_l : out STD_LOGIC_VECTOR (0 downto 0);
-           e : out STD_LOGIC;
-           g : out STD_LOGIC;
-           l : out STD_LOGIC;
-           and : out STD_LOGIC_VECTOR (0 downto 0);
-           or : out STD_LOGIC_VECTOR (0 downto 0);
-           xor : out STD_LOGIC_VECTOR (0 downto 0));
+    Generic(n: integer := 32;
+            shift_n : integer := 5);
+    Port ( r1 : in STD_LOGIC_VECTOR (n-1 downto 0);
+           r2 : in STD_LOGIC_VECTOR (n-1 downto 0);
+           pc : in STD_LOGIC_VECTOR (n-1 downto 0);
+           i : in STD_LOGIC_VECTOR (n-1 downto 0); --instruction
+           q : out STD_LOGIC_VECTOR (n-1 downto 0));
 end alu;
 
 architecture RTL of alu is
 
+    signal a : STD_LOGIC_VECTOR (n-1 downto 0);
+    signal b : STD_LOGIC_VECTOR (n-1 downto 0);
+
+    signal b_eq_rs2: STD_LOGIC;
+
+    component adder_n_bit is
+        Generic (n: integer := 32);
+        Port ( a : in STD_LOGIC_VECTOR (n-1 downto 0);
+               b : in STD_LOGIC_VECTOR (n-1 downto 0);
+               sub : in STD_LOGIC;
+               q : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal adder_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component and_bus is
+        Generic (n: integer := 32);
+        Port ( a : in STD_LOGIC_VECTOR (n-1 downto 0);
+               b : in STD_LOGIC_VECTOR (n-1 downto 0);
+               q : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal and_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component or_bus is
+        Generic (n: integer := 32);
+        Port ( a : in STD_LOGIC_VECTOR (n-1 downto 0);
+               b : in STD_LOGIC_VECTOR (n-1 downto 0);
+               q : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal or_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component xor_bus is
+        Generic (n: integer := 32);
+        Port ( a : in STD_LOGIC_VECTOR (n-1 downto 0);
+               b : in STD_LOGIC_VECTOR (n-1 downto 0);
+               q : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal xor_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component barrel_shift_right is
+        Generic(n: integer := 32;
+                shift_n : integer := 5);
+        Port ( d : in STD_LOGIC_VECTOR (n-1 downto 0);
+               sft : in STD_LOGIC_VECTOR (shift_n-1 downto 0);
+               arith : in STD_LOGIC;
+               z : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal sr_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component barrel_shift_left is
+        Generic(n: integer := 32;
+                shift_n : integer := 5);
+        Port ( d : in STD_LOGIC_VECTOR (n-1 downto 0);
+               sft : in STD_LOGIC_VECTOR (shift_n-1 downto 0);
+               z : out STD_LOGIC_VECTOR (n-1 downto 0));
+    end component;
+
+    signal sl_out : STD_LOGIC_VECTOR(n-1 downto 0);
+
+    component comparator_n_bit is
+        Generic (n: integer := 32);
+        Port ( a : in STD_LOGIC_VECTOR (n-1 downto 0);
+               b : in STD_LOGIC_VECTOR (n-1 downto 0);
+               sign_n_unsign : in STD_LOGIC;
+               g : out STD_LOGIC;
+               e : out STD_LOGIC;
+               l : out STD_LOGIC);
+    end component;
+    
+    signal comp_g : STD_LOGIC;
+    signal comp_e : STD_LOGIC;
+    signal comp_l : STD_LOGIC;
+
 begin
 
+    gen_a: 
+    for j in 0 to n-1 generate
+        a(j) <= (r1(j) AND NOT i(2)) OR (pc(j) AND NOT (NOT i(6) AND i(5)));
+                                                -- NOT LUI 
+    end generate;
+
+
+    b_eq_rs2 <= i(5) AND NOT i(2) AND (i(6) OR i(4));
+
+    gen_b_31_21:
+    for j in 31 to 20 generate
+        b(j) <= (r2(j) AND b_eq_rs2) OR (i(j) AND i(4) AND NOT i(3) AND i(2)) OR i(31) AND NOT b_eq_rs2;
+                --r2                                               --LUI AUIPC                           --Sign extend
+    end generate; 
+
+    for j in 19 to 12 generate
+        b(j) <= (r2(j) AND b_eq_rs2) OR (i(j) AND i(4) AND NOT i(3) AND i(2)) OR i(2) AND i(3) AND i(j) OR i(31) AND NOT b_eq_rs2;
+            --r2                                               --LUI AUIPC                                   --JAL                        --Sign extend
+    end generate; 
+
+    --Not setup yet
+    add_sub: adder_n_bit
+        Generic map(n=>n)
+        Port Map(
+            a => l_not,
+            b => l_bus(0),
+            sel => invert,
+            q => l
+        );
 
 end RTL;
