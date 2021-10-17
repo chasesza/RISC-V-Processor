561c5cbcb5f8c7cbaefc36df00d02a6871bdf537
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-10-16 23:13:23 -0400

    Update assembler.py

--- Python Scripts/assembler.py
+++ Python Scripts/assembler.py
@@ -11,6 +11,7 @@ lines = src.readlines()
 for line in lines:
     d = re.split(", |,| ", line)
     s=''
+    valid = True
     if d[0].lower() == "lui":
         s = format(int(d[2], 16), "020b") #imm
         s += format(int(d[1][1:], 10), "05b") #rd
@@ -25,8 +26,27 @@ for line in lines:
         s = imm[20] + imm[1:11][::-1] + imm[11] + imm[12:20][::-1] #j-type
         s += format(int(d[1][1:], 10), "05b") #rd
         s += "1101111" #opcode
-    s += '\n'
-    dest.write(s)
+    elif d[0].lower() == "jalr":
+        imm = format(int(d[3], 16), "012b") #imm
+        s = imm #i-type
+        s += format(int(d[2][1:], 10), "05b") #rs1
+        s += "000" #func3
+        s += format(int(d[1][1:], 10), "05b") #rd
+        s += "1100111" #opcode
+    elif d[0].lower() == "beq":
+        imm = format(int(d[3], 16), "013b") #imm
+        imm = str(imm)[::-1] #reverse to make indices match
+        s = imm[12] + imm[5:11]  #b-type
+        s += format(int(d[2][1:], 10), "05b") #rs2
+        s += format(int(d[1][1:], 10), "05b") #rs1
+        s += "000" #func3
+        s += imm[1:4] + imm[11] #b-type
+        s += "1100111" #opcode
+    else:
+        valid = False
+    if valid:
+        s += '\n'
+        dest.write(s)
 
 dest.close()
 src.close()
