9acb62afbfa462c9e73cc4c1f1806c0e9e9ab60c
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-10-24 23:59:28 -0400

    Fixed a comment that mislabeled a register

--- Python Scripts/adder.asm
+++ Python Scripts/adder.asm
@@ -8,5 +8,5 @@ andi    x4,     x4,     15      ;18     bitwise and by 0xF to get buttons and st
 add     x6,     x4,     x5      ;1c     add buttons and switches and store in x6
 sw      x6,     x0,     1       ;20     stores data from x6 in block ram address 1
 lw      x7,     x0,     1       ;24     loads data from block ram address 1 to  x7, note that this store/load is solely for testing purposes
-sw      x7,     x3,     0       ;28     stores data from x6 in the led reg
+sw      x7,     x3,     0       ;28     stores data from x7 in the led reg
 jal     x0,     4294967268      ;2c     decrement pc by 28, discard link register
